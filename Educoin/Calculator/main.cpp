b2a079c53ab38efb3a3b2e4fa690a075162d9348
Vladyslav Uslystyi <vladuvi23@gmail.com>
2020-04-21 01:06:17 +0300

    A working program (without intetsive testing)

--- Educoin/Calculator/main.cpp
+++ Educoin/Calculator/main.cpp
@@ -1,35 +1,26 @@
-#include "Lexer.hpp"
-#include "Parser.hpp"
 #include "Executor.hpp"
 
+void run() {
+    Executor        executor;
+    std::string     expression;
+    int_fast64_t    res;
+
+    while (1)
+	{
+		std::getline(std::cin, expression, '\n');
+		if (std::cin.eof()) {
+            std::cout << "Bye" << std::endl;
+			break ;
+        }
+        if (!expression.empty()) {
+		    res = executor.executeExpression(expression);
+            std::cout << res << std::endl;
+        }
+	}
+}
+
 int main() {
-    Executor executor;
 
-    // executor.executeExpression("-((-2 * 4) * ((-10) - 7) )");
-    // executor.executeExpression("$0 - 64");
-
-    // executor.executeExpression("100");
-    executor.executeExpression("1");
-    executor.executeExpression("2");
-    executor.executeExpression("3");
-    executor.executeExpression("4");
-    executor.executeExpression("5");
-    executor.executeExpression("6");
-    executor.executeExpression("7");
-    executor.executeExpression("8");
-    executor.executeExpression("9");
-    executor.executeExpression("10");
-    executor.executeExpression("11");
-
-
-    int_fast64_t res = executor.executeExpression("$0");
-
-    std::cout << res << std::endl;
-
-    // for (auto e : list) {
-    //     std::cout << strToken[e.getToken()] << " ";
-    // }
-    // std::cout << std::endl;
-
+    run();
     return 0;
 }
